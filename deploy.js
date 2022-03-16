b2cda545f59b9dffacd35ea8099cee4a5c3b5288
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2022-03-16 21:51:41 +0300

    Deployment script added

---
+++ deploy.js
@@ -0,0 +1,6 @@
+const moment = require('moment');
+const execSync = require('child_process').execSync;
+
+const dateTime = moment().format('DD/MM/YYYY HH:mm:ss');
+
+execSync(`git add . && git commit -m \"Blog updated: ${dateTime}\" && git push`, { stdio: [0, 1, 2]} );
