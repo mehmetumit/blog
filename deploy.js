23b7a3f36e3d79478b3ccd3a181a669b59c799da
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2022-03-16 23:23:07 +0300

    Blog updated: 16/03/2022 23:23:07

--- deploy.js
+++ deploy.js
@@ -3,4 +3,4 @@ const execSync = require('child_process').execSync;
 
 const dateTime = moment().format('DD/MM/YYYY HH:mm:ss');
 
-execSync(`git add . && git commit -m \"Blog updated: ${dateTime}\" && git push`, { stdio: [0, 1, 2]} );
+execSync(`git add . && git commit -m \"Blog updated: ${dateTime}\" && git push - f`, { stdio: [0, 1, 2]} );
